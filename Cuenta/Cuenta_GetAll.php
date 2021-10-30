1b014050a5402d4e5738074d4d0b7ed7a954edb9
BryanORCc <bryanorcc@outlook.com>
2021-10-30 18:37:16 -0500

    make it better

---
+++ Cuenta/Cuenta_GetAll.php
@@ -0,0 +1,34 @@
+<?php
+	require 'SQLGlobal.php';
+
+	if($_SERVER['REQUEST_METHOD']=='GET'){
+		try{
+			//$id = $_GET['id']; // obtener parametros GET
+			//$respuesta = SQLGlobal::query("QUERY");//sin filtro ("No incluir filtros ni '?'")
+			$respuesta = SQLGlobal::selectArray("select * from cuenta");
+            echo json_encode(array(
+				'respuesta' => '200',
+                'estado' => 'Se obtuvieron los datos correctamente',
+                'data' => $respuesta,
+                'error' => ''
+			));
+            
+            //con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+			echo json_encode(array(
+				'respuesta'=>'200',
+				'estado' => 'Se obtuvieron los datos correctamente',
+				'data'=>$respuesta,
+				'error'=>''
+			));
+		}catch(PDOException $e){
+			echo json_encode(
+				array(
+					'respuesta'=>'-1',
+					'estado' => 'Ocurrio un error, intentelo mas tarde',
+					'data'=>'',
+					'error'=>$e->getMessage())
+			);
+		}
+	}
+
+?>
---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "bhvzeaydhdwknb"); // Usuario
+define('DB_PASSWORD', "66667f46324a988e886b495a32242266fd2cf6991455fae39176faa58d0e388b"); // Password
+define('DB_DATABASE', "d2qf2duvtigfhn"); // Nombre de la base de datos
+define('DB_SERVER', "ec2-3-91-127-228.compute-1.amazonaws.com"); //host server
+?>
