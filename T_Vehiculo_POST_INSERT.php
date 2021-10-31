be6ef143c5d3c290cfc894355a110cc550b9d09a
BryanORCc <bryanorcc@outlook.com>
2021-10-30 19:53:12 -0500

    make it better

--- T_Vehiculo_POST_INSERT.php
+++ T_Vehiculo_POST_INSERT.php
@@ -11,12 +11,23 @@
             $ModeloVehiculo = $datos["ModeloVehiculo"];
 			$respuesta = SQLGlobal::cudFiltro("INSERT into T_Vehiculo values (?,?,?,?,?)",array($VehiculoId, $TipoVehiculo, $FotoVehiculo, $MarcaVehiculo, $ModeloVehiculo));
 
-			echo json_encode(array(
-				'respuesta'=>'200',
-				'estado' => 'Se obtuvieron los datos correctamente',
-				'data'=>$respuesta,
-				'error'=>''
-			));
+            if($respuesta>0){
+                echo json_encode(array(
+                    'respuesta'=>'200',
+                    'estado' => 'Registro exitoso',
+                    'data'=> 'El numero de registros afectados es: '.$respuesta,
+                    'error'=>''
+                ));
+            }else{
+                echo json_encode(array(
+                    'respuesta'=>'100',
+                    'estado' => 'Fallo el registro',
+                    'data'=> 'El numero de registros afectados es: '.$respuesta,
+                    'error'=>''
+                ));
+            }
+
+			
 		}catch(PDOException $e){
 			echo json_encode(
 				array(
